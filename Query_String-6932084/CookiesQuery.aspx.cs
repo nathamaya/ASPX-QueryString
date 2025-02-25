ec433a5a83022ca56f332ebe88106ddc61ea5083
nathamaya <nathamayaa@outlook.com>
2025-02-24 22:45:01 -0600

    Primer commit en Query String

---
+++ Query_String-6932084/CookiesQuery.aspx.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Query_String_6932084
+{
+    public partial class CookiesQuery : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnConfirm_Click(object sender, EventArgs e)
+        {
+
+            // Save the cookies.
+            Response.Cookies["ddlCategory"].Value = ddlCategory.SelectedValue;
+            Response.Cookies["ddlSupplier"].Value = ddlSupplier.SelectedValue;
+            Response.Cookies["strProduct"].Value = txtProduct.Text;
+            Response.Cookies["strDescription"].Value = txtDescription.Text;
+            Response.Cookies["strImage"].Value = txtImage.Text;
+            Response.Cookies["decPrice"].Value = txtPrice.Text;
+            Response.Cookies["bytNumberInStock"].Value = txtNumberInStock.Text;
+            Response.Cookies["bytNumberOnOrder"].Value = txtNumberOnOrder.Text;
+            Response.Cookies["bytReorderLevel"].Value = txtReorderLevel.Text;
+            // Go to the confirmation page.
+            Response.Redirect("ProductConfirmCookies.aspx");
+
+            // Go to the confirmation page with the encoded URL.
+            Response.Redirect
+                (
+                    "ProductConfirmQueryStrings.aspx" +
+                    "?ddiCategory=" + ddlCategory.SelectedValue +
+                    "&ddiSupplier=" + ddlSupplier.SelectedValue +
+                    "&strProduct=" + txtProduct.Text +
+                    "&strDescription=" + txtDescription.Text +
+                    "&strImage=" + txtImage.Text +
+                    "&decPrice=" + txtPrice.Text +
+                    "&bytNumberInStock=" + txtNumberInStock.Text +
+                    "&bytNumberOnOrder=" + txtNumberOnOrder.Text +
+                    "&bytReorderLevel=" + txtReorderLevel.Text
+                ) ;
+        }
+    }
+}
---
+++ Query_String-6932084/ProductConfirmCookies.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Query_String_6932084
+{
+    public partial class ProductConfirmCookies : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            // Retrieve the cookies.
+            ddlCategory.SelectedValue = Request.Cookies["ddlCategory"].Value;
+            ddlSupplier.SelectedValue = Request.Cookies["ddlSupplier"].Value;
+            lblProduct.Text = Request.Cookies["strProduct"].Value;
+            txtDescription.Text = Request.Cookies["strDescription"].Value;
+            lblImage.Text = Request.Cookies["strImage"].Value;
+            Decimal decPrice =
+            Convert.ToDecimal(Request.Cookies["decPrice"].Value);
+            lblPrice.Text = decPrice.ToString("c");
+            lblNumberInStock.Text = Request.Cookies["bytNumberInStock"].Value;
+            lblNumberOnOrder.Text = Request.Cookies["bytNumberOnOrder"].Value;
+            lblReorderLevel.Text = Request.Cookies["bytReorderLevel"].Value;
+            // Compute and display the value in stock and the value on order.
+            Byte bytNumberInStock =
+            Convert.ToByte(Request.Cookies["bytNumberInStock"].Value);
+            Byte bytNumberOnOrder =
+            Convert.ToByte(Request.Cookies["bytNumberOnOrder"].Value);
+            Decimal deValueInStock = decPrice * bytNumberInStock;
+            Decimal deValueOnOrder = decPrice * bytNumberOnOrder;
+            lblValueInStock.Text = deValueInStock.ToString("c");
+            lblValueOnOrder.Text = deValueOnOrder.ToString("c");
+
+
+
+        }
+    }
+}
